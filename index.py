4ed180ca6d2efc4886c040ebad89eb93ba5f1e63
Lambert D-T <lambertdt@hotmail.com>
2017-06-12 11:26:08 -0400

    working gitignore

--- index.py
+++ index.py
@@ -12,6 +12,8 @@ import re
 import json
 
 app = Flask(__name__)
+maxNumberOfLevelsID = 10
+maxNumberOfKeywords = 15
 
 @app.route('/')
 def home_page():
@@ -21,7 +23,34 @@ def home_page():
 def level1(counterLevel1):
     return render_template("newLevel1.html", counterLevel1=counterLevel1)
 
+@app.route('/level2/<counterLevel2>')
+def level2(counterLevel2):
+    return render_template("newLevel2.html", counterLevel2=counterLevel2)
+
 @app.route('/submit', methods=['POST'])
 def submit():
+    jsonData = []
+    for x in range(1, maxNumberOfLevelsID):
+        data = {}
+        level1name = request.form.get(str(x) + 'Level1Nom', None)
 
-    return "", 201
+        tablevel1keyword = []
+        level1keyword = request.form.get(str(x) + 'Level1Keyword', None)
+        if level1keyword:
+            tablevel1keyword = level1keyword.split(',')
+
+        level1action = request.form.get(str(x) + 'Level1Action', None)
+
+        if level1name and tablevel1keyword: # if level1, keyword entered
+            if level1action: # avec action
+                print level1name + level1action
+                print tablevel1keyword
+                data[str(x) + 'Level1Name'] = level1name# premier objet json
+                data[str(x) + 'level1Action'] = level1action
+                data[str(x) + 'Level1Keywords'] = json.dumps(tablevel1keyword)
+                jsonData.append(data)
+            else: # sans action (level 2)
+
+                print "sans action"
+    finalJson = json.dumps(jsonData)
+    return finalJson, 201
--- static/js/javascript.js
+++ static/js/javascript.js
@@ -21,6 +21,27 @@ function addLevel1(clickedId) {
 
 }
 
+function addLevel2(clickedId) {
+		counterLevel2++;
+		var xhr = new XMLHttpRequest();
+
+		xhr.onreadystatechange = function() {
+				if (xhr.readyState === XMLHttpRequest.DONE) {
+						if (xhr.status === 200) {
+								var tab = document.getElementById('tableLevel1');
+								var row = tab.insertRow(-1)
+								row.innerHTML = xhr.responseText;
+						} else {
+								console.log('Erreur avec le serveur');
+						}
+				}
+		};
+
+		xhr.open("GET", "/level2/" + counterLevel2, true);
+    xhr.send();
+
+}
+
 function removeLevel1(clickedId) {
 		var elem = document.getElementById(clickedId).parentElement.parentElement;
 		elem.parentNode.removeChild(elem);
